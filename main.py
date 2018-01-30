52f07f9fb355419a7977a10833963699ba354d33
Simon Hohl <simon.hohl@dainst.org>
2018-01-30 12:02:42 +0100

    Add MongoDB connection test

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.debug = True
@@ -6,8 +9,20 @@ app.debug = True
 
 @app.route('/')
 def index():
-    app.logger.debug('Hier spricht der Debugger.')
+    app.logger.debug('Hello world! Says the debug logger...')
+
     return 'Hello world!'
+
+
+@app.route('/database_info')
+def database_info():
+    credentials = os.environ['DB_ROOT_CREDENTIALS']
+    host = os.environ['DB_HOST']
+    mongo_client = MongoClient('mongodb://' + credentials + '@' + host)
+
+    app.logger.debug(mongo_client.server_info())
+
+    return jsonify(mongo_client.server_info())
 
 
 if __name__ == '__main__':
