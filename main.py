3f398aa4a6819471638e9e2f49aa86efd0302bfb
Simon Hohl <simon.hohl@dainst.org>
2018-02-06 15:09:13 +0100

    Add route /annotate/<operation>

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from idai_journals.publications import TextAnalyzer
 
 app = Flask(__name__)
 app.debug = True
-
 
 @app.route('/')
 def index():
     app.logger.debug('Hello world! Says the debug logger...')
 
-    return 'Hello world!'
+    return 'Hello Wolfgang!'
+
+
+@app.route('/annotate/<operation>', methods=['POST'])
+def annotate(operation):
+
+    request.get_data()
+
+    input_text = request.data.decode('utf-8')
+
+    result = TextAnalyzer(str(input_text))
+    content = None
+
+    if operation == "NER":
+        content = jsonify(result.NER())
+    elif operation == "POS":
+        content = jsonify(result.pos_tag())
+    elif operation == "ALL":
+        content = \
+            jsonify(dict ({
+                "NER": result.NER(),
+                "POS": result.pos_tag()
+            }))
+    else:
+        content = 'Invalid operation: %s' % operation
+        return content, 400
+
+    return content, 200
 
 
 @app.route('/database_info')
