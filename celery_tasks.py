3623fc4c070205112451d995261e01718d3da68c
Simon Hohl <simon.hohl@dainst.org>
2018-03-19 16:53:02 +0100

    Switched all current routes to celery setup

--- celery_tasks.py
+++ celery_tasks.py
@@ -1,5 +1,3 @@
-import time
-import random
 import os
 
 from celery import Celery
@@ -21,44 +19,52 @@ def annotate(self, input_text, params):
     text_analyzer = TextAnalyzer(input_text)
     if 'lang' in params:
         text_analyzer.lang = params['lang']
-
-    result = _run_operation(text_analyzer, params)
+    self.update_state(state='PROGRESS',
+                      meta={'status': 'Running NLP analysis...'})
+    result = _run_annotation(text_analyzer, params)
     result = _add_metadata(result, text_analyzer)
 
-    return {'result': result}
+    return {'result': result, 'status': 'Processing completed.'}
 
 
-@celery.task(bind=True, name='long_task')
-def long_task(self):
-    """Background task that runs a long function with progress reports."""
-    verbs = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
-    adjectives = ['master', 'radiant', 'silent', 'harmonic', 'fast']
-    nouns = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+@celery.task(bind=True, name='get_entities')
+def get_entities(self, input_text, params):
+    text_analyzer = TextAnalyzer(input_text)
+    if 'lang' in params:
+        text_analyzer.lang = params['lang']
+    self.update_state(state='PROGRESS',
+                      meta={'status': 'Running NLP analysis...'})
 
-    message = ''
+    # TODO: This is somewhat ugly.
+    # We should think about changing the Flask endpoint: You should be able to pass a list
+    # of wanted entity types, which are parsed in Flask and passed to the worker as a list in the params dict.
+    # Then we could just replace `exclusive_entity_type` by `params['requested_entity_types_list']`
+    # or something similar here.
+    if params['entity_type'] is not None:
+        exclusive_entity_type = [text_analyzer.SUPPORTED_ENTITY_TYPES[params["entity_type"]]]
+    else:
+        exclusive_entity_type = []
 
-    total = random.randint(10, 50)
-    for i in range(total):
-        if not message or random.random() < 0.25:
-            message = '{0} {1} {2}...'.format(random.choice(verbs),
-                                              random.choice(adjectives),
-                                              random.choice(nouns))
-        self.update_state(state='PROGRESS',
-                          meta={'current': i, 'total': total, 'status': message})
+    entity_list = \
+        text_analyzer.get_entities(
+            text_analyzer.do_ner(),
+            exclusive_entity_type,
+            params['include_references'])
 
-        time.sleep(1)
+    result = {'entity_list': [entity.to_json() for entity in entity_list]}
+    result = _add_metadata(result, text_analyzer)
 
-    return {'current': 100, 'total': 100, 'status': 'Task completed!', 'result': 42}
+    return {'result': result, 'status': 'Processing completed.'}
 
 
-def _run_operation(text_analyzer, params):
+def _run_annotation(text_analyzer, params):
 
     result = dict()
 
     if 'NER' in params['operation']:
         result['named_entities'] = text_analyzer.do_ner()
     if 'POS' in params['operation']:
-        result['part_of_speech__tags'] = text_analyzer.do_pos_tag()
+        result['part_of_speech_tags'] = text_analyzer.do_pos_tag()
 
     return result
 
@@ -72,3 +78,27 @@ def _add_metadata(result, text_analyzer):
             "sentence_count": len(text_analyzer.sentences)
         })
     }
+
+
+# old tutorial code for celery
+# @celery.task(bind=True, name='long_task')
+# def long_task(self):
+#     """Background task that runs a long function with progress reports."""
+#     verbs = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
+#     adjectives = ['master', 'radiant', 'silent', 'harmonic', 'fast']
+#     nouns = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+#
+#     message = ''
+#
+#     total = random.randint(10, 50)
+#     for i in range(total):
+#         if not message or random.random() < 0.25:
+#             message = '{0} {1} {2}...'.format(random.choice(verbs),
+#                                               random.choice(adjectives),
+#                                               random.choice(nouns))
+#         self.update_state(state='PROGRESS',
+#                           meta={'current': i, 'total': total, 'status': message})
+#
+#         time.sleep(1)
+#
+#     return {'current': 100, 'total': 100, 'status': 'Task completed!', 'result': 42}
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-import copy
 from classes.service_error import ServiceError
-from idai_journals.publications import TextAnalyzer
 
 from flask import Flask, jsonify, request, url_for
 from celery_client import celery
@@ -9,8 +6,9 @@ from celery_client import celery
 app = Flask('nlp_service')
 app.debug = True
 
-KNOWN_OPERATIONS = ["NER", "POS"]
-KNOWN_LANGUAGES = ['de', 'en', 'it', 'fr']
+VALID_ANNOTATION_OPERATIONS = ["NER", "POS"]
+VALID_ENTITY_TYPES = ["locations", "persons", "organisations"]  # as defined in nlp_components as SUPPORTED_ENTITY_TYPES
+VALID_LANGUAGES = ['de', 'en', 'it', 'fr']
 DEFAULT_PARAMS = {
     "include_references": True
 }
@@ -18,172 +16,58 @@ DEFAULT_PARAMS = {
 
 @app.route('/')
 def index():
-    app.logger.debug('Hello world! Says the debug logger...')
+    app.logger.debug('Hello Wolfgang! Says the debug logger...')
     return 'Hello Wolfgang!'
-
-
-@app.route('/annotate_celery/<operation>', methods=['POST'])
-def annotate_celery(operation):
-    try:
-        request.get_data()
-        input_text = request.data.decode('utf-8')
-        params = _parse_request_params(request.args)
-        params['operation'] = _parse_annotation_operation(operation)
-        task = celery.send_task('annotate', args=[input_text, params], kwargs={})
-
-        return jsonify({}), 202, {'Location': url_for('task_status', task_id=task.id)}
-    except ServiceError as error:
-        return error.build()
-
-
-def _parse_request_params(request_params):
-    params = copy.copy(DEFAULT_PARAMS)
-    if "lang" in request_params:
-        if request.args["lang"] in KNOWN_LANGUAGES:
-            params['lang'] = request_params['lang']
-        else:
-            raise ServiceError("Language '%s' not supported." % request_params["lang"])
-
-    if "include-references" in request_params:
-        if request_params["include-references"] == "True":
-            params["include_references"] = True
-        elif request_params["include-references"] == "False":
-            params["include_references"] = False
-        else:
-            params = DEFAULT_PARAMS["include_references"]
-
-    return params
-
-
-def _parse_annotation_operation(requested_operation):
-
-    if requested_operation == 'all':
-        return KNOWN_OPERATIONS
-
-    if requested_operation in KNOWN_OPERATIONS:
-        return requested_operation
-    else:
-        raise ServiceError('Unknown operation: %s.' % requested_operation, 400)
 
 
 @app.route('/annotate/<operation>', methods=['POST'])
 def annotate(operation):
     try:
-        text_analyzer = _create_text_analyzer(request)
-        content = dict({})
-        params = copy.copy(DEFAULT_PARAMS)
-        params["operation"] = operation
-        _set_up_params_and_text_analyzer(request, params, text_analyzer)
-        _run_operation(content, params, text_analyzer)
-        _add_metadata(content, params, text_analyzer)
-        return jsonify(content)
+        request.get_data()
+        input_text = request.data.decode('utf-8')
+        params = _parse_request_args(request.args)
+        params['operation'] = _parse_annotation_operation(operation)
+        task = celery.send_task('annotate', args=[input_text, params], kwargs={})
+
+        return jsonify('Accepted'),\
+            202, {'Location': url_for('task_status', task_id=task.id)}
+
     except ServiceError as error:
         return error.build()
 
 
-@app.route('/get-entities/', methods=['POST'], defaults={'entity_type': ""})
+@app.route('/get-entities/', methods=['POST'], defaults={'entity_type': None})
 @app.route('/get-entities/<entity_type>', methods=['POST'])
 def get_entities(entity_type):
     try:
-        text_analyzer = _create_text_analyzer(request)
-        content = dict({})
-        params = copy.copy(DEFAULT_PARAMS)
-        params["entity_type"] = entity_type
-        _set_up_params_and_text_analyzer(request, params, text_analyzer)
-        _get_entities_from_text(content, params, text_analyzer)
-        _add_metadata(content, params, text_analyzer)
-        return jsonify(content)
+        request.get_data()
+        input_text = request.data.decode('utf-8')
+        params = _parse_request_args(request.args)
+        params['entity_type'] = _validate_entity_type(entity_type)
+
+        task = celery.send_task('get_entities', args=[input_text, params], kwargs={})
+
+        return jsonify('Accepted'),\
+            202, {'Location': url_for('task_status', task_id=task.id)}
+
     except ServiceError as error:
         return error.build()
-
-
-@app.route('/list-languages')
-def list_languages():
-    # @ TODO get drom TextAnalyzer
-    return jsonify(KNOWN_LANGUAGES)
-
-
-def _get_entities_from_text(content, params, text_analyzer):
-    list = text_analyzer.get_entities(text_analyzer.do_ner(), params["entity_type"], params["include_references"])
-    content["entities"] = [e.to_json() for e in list]
-
-
-def _create_text_analyzer(request):
-    request.get_data()
-    input_text = request.data.decode('utf-8')
-    if len(input_text) < 30:
-        raise ServiceError('Input is not long enough to parse. It must be at least 30 characters', 400)
-    return TextAnalyzer(str(input_text))
-
-
-def _run_operation(content, params, textAnalyzer):
-    operation = params["operation"]
-    if operation not in KNOWN_OPERATIONS:
-        operation = "all"
-    content["operation"] = operation
-    if operation in ["NER", "all"]:
-        content["named_entities"] = textAnalyzer.do_ner()
-    if operation in ["POS", "all"]:
-        content["part_of_speech_tags"] = textAnalyzer.do_pos_tag()
-    return content
-
-
-def _add_metadata(content, params, textAnalyzer):
-    content["meta"] = dict({
-        "detected_language": textAnalyzer.lang,
-        "word_count": len(textAnalyzer.words),
-        "sentence_count": len(textAnalyzer.sentences)
-    })
-
-
-def _set_up_params_and_text_analyzer(request, params, text_analyzer):
-    if "lang" in request.args:
-        if request.args["lang"] in KNOWN_LANGUAGES:
-            text_analyzer.lang = request.args["lang"]
-        else:
-            raise ServiceError("language '%s' not supported." % request.args["lang"])
-    if "include-references" in request.args:
-        params["include_references"] = _parameter_to_boolean(request.args["include-references"], params["include_references"])
-    if "entity_type" in params:
-        if params["entity_type"] == "":
-            params["entity_type"] = []
-        elif params["entity_type"] not in text_analyzer.SUPPORTED_ENTITY_TYPES.keys():
-            raise ServiceError("entity type '%s' not supported!" % params["entity_type"])
-        else:
-            params["entity_type"] = [text_analyzer.SUPPORTED_ENTITY_TYPES[params["entity_type"]]]
-
-
-def _parameter_to_boolean(param, default_value):
-    if param in ["false", "False", "FALSE", "0", "Off", "off", "OFF", "NO", "no", "No", 0, False]:
-        return False
-    if param in ["true", "True", "TRUE", "1", "On", "on", "ON", "YES", "yes", "Yes", 1, True]:
-        return True
-    return default_value
-
-
-@app.route('/longtask', methods=['POST'])
-def long_task():
-    task = celery.send_task('long_task', args=[], kwargs={})
-    app.logger.debug(task.id)
-    return jsonify({}), 202, {'Location': url_for('task_status', task_id=task.id)}
 
 
 @app.route('/status/<task_id>')
 def task_status(task_id):
     task = celery.AsyncResult(task_id)
     if task.state == 'PENDING':
-        # job did not start yet
         response = {
             'state': task.state,
-            'current': 0,
-            'total': 1,
-            'status': 'Pending...'
+            'status':
+                'Task is waiting for execution or unknown. '
+                'Any task id that’s not known is implied to be in the pending state. See: '
+                'http://docs.celeryproject.org/en/latest/userguide/tasks.html#states'
         }
     elif task.state != 'FAILURE':
         response = {
             'state': task.state,
-            'current': task.info.get('current', 0),
-            'total': task.info.get('total', 1),
             'status': task.info.get('status', '')
         }
         if 'result' in task.info:
@@ -192,12 +76,74 @@ def task_status(task_id):
         # something went wrong in the background job
         response = {
             'state': task.state,
-            'current': 1,
-            'total': 1,
             'status': str(task.info),  # this is the exception raised
         }
     return jsonify(response)
 
 
+@app.route('/list_options/', methods=['GET'], defaults={'option': None})
+@app.route('/list_options/<option>', methods=['GET'])
+def list_options(option):
+    if option == 'annotation-operations':
+        return jsonify(VALID_ANNOTATION_OPERATIONS)
+    elif option == 'languages':
+        return jsonify(VALID_LANGUAGES)
+    elif option == 'entity-types':
+        return jsonify(VALID_ENTITY_TYPES)
+    else:
+        return jsonify({
+            'annotation-operations': VALID_ANNOTATION_OPERATIONS,
+            'languages': VALID_LANGUAGES,
+            'entity-types': VALID_ENTITY_TYPES,
+        })
+
+
+def _parse_request_args(args):
+    params = {}
+    if "lang" in args:
+        if request.args["lang"] in VALID_LANGUAGES:
+            params['lang'] = args['lang']
+        else:
+            raise ServiceError("Language '%s' not supported." % args["lang"])
+
+    if "include-references" in args:
+        if args["include-references"] == "True":
+            params["include_references"] = True
+        elif args["include-references"] == "False":
+            params["include_references"] = False
+        else:
+            params["include_references"] = DEFAULT_PARAMS["include_references"]
+    else:
+        params["include_references"] = DEFAULT_PARAMS["include_references"]
+
+    return params
+
+
+def _parse_annotation_operation(requested_operation):
+
+    if requested_operation == 'all':
+        return VALID_ANNOTATION_OPERATIONS
+
+    if requested_operation in VALID_ANNOTATION_OPERATIONS:
+        return requested_operation
+    else:
+        raise ServiceError('Unknown operation: %s.' % requested_operation, 400)
+
+
+def _validate_entity_type(entity_type):
+    if entity_type not in VALID_ENTITY_TYPES and entity_type is not None:
+        raise ServiceError("Entity type '%s' not supported." % entity_type)
+    return entity_type
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
+# old tutorial code for celery
+# @app.route('/longtask', methods=['POST'])
+# def long_task():
+#     task = celery.send_task('long_task', args=[], kwargs={})
+#     app.logger.debug(task.id)
+#     return jsonify({}), 202, {'Location': url_for('task_status', task_id=task.id)}
+#
